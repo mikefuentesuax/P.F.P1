6a882f4470a3c054d8733cfbdd8212dbc06b55c9
mikefuentesuax <mmurimer@myuax.com>
2024-05-18 16:55:20 +0200

    Creacion de la clase poblacion que contiene los datos relaciones al experimetno de las bacterias y los calculos necesarios para la dosis de comida

--- P.F.P1/src/main/java/org/example/PoblacionBacterias.java
+++ P.F.P1/src/main/java/org/example/PoblacionBacterias.java
@@ -1,4 +1,123 @@
-package org.example;
+import java.io.Serializable;
+import java.util.Date;
 
-public class PoblacionBacterias {
+public class PoblacionBacterias implements Serializable {
+    private String nombre;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private int numBacteriasIniciales;
+    private int temperatura;
+    private String condicionesLuminosidad;
+    private int dosisComidaInicial;
+    private int diaIncrementoComida;
+    private int comidaDiaIncremento;
+    private int comidaFinalDia30;
+
+    public PoblacionBacterias(String nombre, Date fechaInicio, Date fechaFin, int numBacteriasIniciales,
+                              int temperatura, String condicionesLuminosidad, int dosisComidaInicial,
+                              int diaIncrementoComida, int comidaDiaIncremento, int comidaFinalDia30) {
+        this.nombre = nombre;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.numBacteriasIniciales = numBacteriasIniciales;
+        this.temperatura = temperatura;
+        this.condicionesLuminosidad = condicionesLuminosidad;
+        this.dosisComidaInicial = dosisComidaInicial;
+        this.diaIncrementoComida = diaIncrementoComida;
+        this.comidaDiaIncremento = comidaDiaIncremento;
+        this.comidaFinalDia30 = comidaFinalDia30;
+    }
+
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public int getNumBacteriasIniciales() {
+        return numBacteriasIniciales;
+    }
+
+    public void setNumBacteriasIniciales(int numBacteriasIniciales) {
+        this.numBacteriasIniciales = numBacteriasIniciales;
+    }
+
+    public int getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(int temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public String getCondicionesLuminosidad() {
+        return condicionesLuminosidad;
+    }
+
+    public void setCondicionesLuminosidad(String condicionesLuminosidad) {
+        this.condicionesLuminosidad = condicionesLuminosidad;
+    }
+
+    public int getDosisComidaInicial() {
+        return dosisComidaInicial;
+    }
+
+    public void setDosisComidaInicial(int dosisComidaInicial) {
+        this.dosisComidaInicial = dosisComidaInicial;
+    }
+
+    public int getDiaIncrementoComida() {
+        return diaIncrementoComida;
+    }
+
+    public void setDiaIncrementoComida(int diaIncrementoComida) {
+        this.diaIncrementoComida = diaIncrementoComida;
+    }
+
+    public int getComidaDiaIncremento() {
+        return comidaDiaIncremento;
+    }
+
+    public void setComidaDiaIncremento(int comidaDiaIncremento) {
+        this.comidaDiaIncremento = comidaDiaIncremento;
+    }
+
+    public int getComidaFinalDia30() {
+        return comidaFinalDia30;
+    }
+
+    public void setComidaFinalDia30(int comidaFinalDia30) {
+        this.comidaFinalDia30 = comidaFinalDia30;
+    }
+
+    // Métodos para cálculos relacionados con la dosis de comida
+    public int calcularComidaDia(int dia) {
+        if (dia < 1 || dia > 30) {
+            throw new IllegalArgumentException("Día inválido");
+        }
+        if (dia <= diaIncrementoComida) {
+            return dosisComidaInicial + (dia - 1) * (comidaDiaIncremento - dosisComidaInicial) / diaIncrementoComida;
+        } else {
+            return comidaDiaIncremento + (dia - diaIncrementoComida) * (comidaFinalDia30 - comidaDiaIncremento) / (30 - diaIncrementoComida);
+        }
+    }
 }
